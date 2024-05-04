9bd9ee9998dc2658413bad3d29d9aa692a49c118
rafael-isidro <rafa-isidro@hotmail.com>
2024-05-03 22:02:29 -0300

    Feat: deleted unused import from SecurityConfig class

--- src/main/java/com/dev/api/petfeliz/infra/security/SecurityConfig.java
+++ src/main/java/com/dev/api/petfeliz/infra/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.dev.api.petfeliz.infra.security;
 
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
